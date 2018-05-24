cad1fd338d7957f99ce8961133a6d55901830742
olgabot <olga.botvinnik@gmail.com>
2018-05-24 15:37:27 -0700

    Use proper filename!!

--- embedH5.py
+++ embedH5.py
@@ -72,7 +72,7 @@ def convert(in_filename, label_filename, outfile, mapper, worddim,
         # This is actually a fasta file, so convert to a table
         # for future use
         seqs = pd.DataFrame([[record.id, str(record.seq)] for record in
-                             SeqIO.parse('example/test.fa', 'fasta')])
+                             SeqIO.parse(in_filename, 'fasta')])
 
     target = pd.read_table(label_filename, header=None, sep='\s+')
 
